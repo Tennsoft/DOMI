ca742a8c6df7ee1c8785bf3db0dbf0b60914a507
Ryan <trjtry@gmail.com>
2020-01-28 12:37:07 -0500

    implemented initial end screen, routing and linked endscreen to homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { EndScreenComponent } from './end-screen/end-screen.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  //{ path:"/dungeon", },
+  { path: "endScreen", component: EndScreenComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
