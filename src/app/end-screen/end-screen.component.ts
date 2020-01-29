dfadf126a12ef84537c439c911044df941650c56
Ryan <trjtry@gmail.com>
2020-01-29 17:35:36 -0500

    added main background and components for inventory and current room

--- src/app/end-screen/end-screen.component.ts
+++ src/app/end-screen/end-screen.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EndScreenComponent implements OnInit {
 
-  EndMessage = "You Survived The Dungeon";
+  EndMessage = "";
+
+  winMessage = "<h4 class='text-primary'>You Survived The Dungeon<h4>";
+  loseMessage = "<h4 class='text-danger'>You Died<h4>";
+  tieMessage = "<h4 class='text-warning'>You defeated the Boss but at what cost?<h4>";
+
+  temp = [this.winMessage,this.loseMessage,this.tieMessage];
 
   constructor() { }
 
   ngOnInit() {
+    
+    this.EndMessage = this.temp[Math.floor(Math.random()*3)];
   }
 }
