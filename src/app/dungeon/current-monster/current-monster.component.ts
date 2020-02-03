a0399f84ba9dfdefe406c9c2e8c95f0a38078c27
TessellatedDaisy <winterhavenc@gmail.com>
2020-02-03 13:31:58 -0500

    search for treasure button display and minor updates

--- src/app/dungeon/current-monster/current-monster.component.ts
+++ src/app/dungeon/current-monster/current-monster.component.ts
@@ -22,10 +22,13 @@ export class CurrentMonsterComponent implements OnInit {
   //initialize
   current_room: {x: number, y: number};
   current_room_abs_id;
+  current_monster_base_name;
   current_monster_name;
   current_monster_desc;
   boss_monster_name;
   boss_monster_desc;
+
+  can_search_for_treasure = this.playerArrayService.getSearchPossible();
 
   constructor(public playerArrayService: PlayerArrayService,public move_room_service: MoveRoomService, public monster_layout_service: MonsterLayoutService) { 
     //console.log(this.monster_list);
@@ -37,8 +40,15 @@ export class CurrentMonsterComponent implements OnInit {
      //turn that into a single number to get an index from the rooms array
      this.current_room_abs_id = this.move_room_service.current_room_reduce();
      //and pull a monster for where you happen to be
+     this.current_monster_base_name = this.monster_list[this.current_room_abs_id].name;
      this.current_monster_name = this.monster_list[this.current_room_abs_id].namePretty;
      this.current_monster_desc = this.monster_list[this.current_room_abs_id].description;
+
+     if(this.current_monster_base_name == "treasure_find"){
+       this.can_search_for_treasure = true;
+       this.playerArrayService.setSearchPossible(true);
+       console.log(this.playerArrayService.getSearchPossible());
+     }
      
      //console.log(this.playerArrayService.getPosition());
 
@@ -55,8 +65,16 @@ export class CurrentMonsterComponent implements OnInit {
       this.current_monster_name = this.boss_list[1].namePretty;
       this.current_monster_desc = this.boss_list[1].description;
     }else{
+      this.can_search_for_treasure = false;
+      this.playerArrayService.setSearchPossible(false);
+    this.current_monster_base_name = this.monster_list[this.current_room_abs_id].name;
     this.current_monster_name = this.monster_list[this.current_room_abs_id].namePretty;
     this.current_monster_desc = this.monster_list[this.current_room_abs_id].description;
+    if(this.current_monster_base_name == "treasure_find"){
+      this.can_search_for_treasure = true;
+      this.playerArrayService.setSearchPossible(true);
+      console.log(this.playerArrayService.getSearchPossible());
+    }
     console.log("current monster is "+ this.current_monster_name);
     }
   }
