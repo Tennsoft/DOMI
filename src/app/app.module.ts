3ac327bfbebc7c6c770b4ec1040344576159e29d
Ryan <trjtry@gmail.com>
2020-02-10 11:25:26 -0500

    deleted room components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DungeonComponent } from './dungeon/dungeon.component';
-import { StorageRoomComponent } from './rooms/storage-room/storage-room.component';
-import { CampRoomComponent } from './rooms/camp-room/camp-room.component';
-import { StalactiteRoomComponent } from './rooms/stalactite-room/stalactite-room.component';
-import { LakeRoomComponent } from './rooms/lake-room/lake-room.component';
-import { OrbitalControlRoomComponent } from './rooms/orbital-control-room/orbital-control-room.component';
-import { DungeonRoomComponent } from './rooms/dungeon-room/dungeon-room.component';
-import { DragonCaveComponent } from './rooms/dragon-cave/dragon-cave.component';
 import { EndScreenComponent } from './end-screen/end-screen.component';
 import { MoveRoomService } from './move-room.service';
 import { CurrentRoomComponent } from './dungeon/current-room/current-room.component';
@@ -32,13 +25,6 @@ import { DevBoxComponent } from './dev-box/dev-box.component';
   declarations: [
     AppComponent,
     DungeonComponent,
-    StorageRoomComponent,
-    CampRoomComponent,
-    StalactiteRoomComponent,
-    LakeRoomComponent,
-    OrbitalControlRoomComponent,
-    DungeonRoomComponent,
-    DragonCaveComponent,
     HomeComponent,
     EndScreenComponent,
     CurrentRoomComponent,
