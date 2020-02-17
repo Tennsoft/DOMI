89716c436e9c9e3639b3991f6a2480069eda5d2f
TessellatedDaisy <winterhavenc@gmail.com>
2020-02-17 10:41:17 -0500

    further updates to moving with 4x4 layout

--- src/app/dungeon/current-room/current-room.component.ts
+++ src/app/dungeon/current-room/current-room.component.ts
@@ -9,6 +9,7 @@ import { rooms } from '../../../../assets/rooms.json';
 import { PlayerArrayService } from '../../player-array.service';
 import { MoveRoomService } from 'src/app/move-room.service';
 import { RoomLayoutService } from 'src/app/room-layout.service';
+import { FourByFourMoveRoomService } from 'src/app/four-by-four-move-room.service';
 
 
 
@@ -31,7 +32,10 @@ export class CurrentRoomComponent implements OnInit, DoCheck {
   current_room_desc;
   
 
-  constructor(public move_room_service: MoveRoomService, public room_layout_service: RoomLayoutService, public playerArrayService: PlayerArrayService) { 
+  constructor(public move_room_service: MoveRoomService, 
+              public room_layout_service: RoomLayoutService, 
+              public playerArrayService: PlayerArrayService,
+              public four_by_four_move_room_service: FourByFourMoveRoomService) { 
 
   }
   
@@ -43,7 +47,13 @@ export class CurrentRoomComponent implements OnInit, DoCheck {
     //get the (x,y) coords
     this.current_room = this.playerArrayService.getPosition();
     //turn that into a single number to get an index from the rooms array
-    this.current_room_abs_id = this.move_room_service.current_room_reduce();
+    if(window.history.state.difficulty == "easy"){
+      this.current_room_abs_id = this.move_room_service.current_room_reduce();
+    }else{
+      this.current_room_abs_id = this.four_by_four_move_room_service.current_room_reduce();
+    }
+    //this.current_room_abs_id = this.move_room_service.current_room_reduce();
+    
     //and pull a room for where you happen to be
     this.current_room_name = this.room_list[this.current_room_abs_id].namePretty;
     this.current_room_desc = this.room_list[this.current_room_abs_id].description;
@@ -67,8 +77,14 @@ export class CurrentRoomComponent implements OnInit, DoCheck {
   ngDoCheck(){
     this.current_room = this.playerArrayService.getPosition();
     //console.log("current room is "+this.current_room_name);
+
+    if(window.history.state.difficulty == "easy"){
+      this.current_room_abs_id = this.move_room_service.current_room_reduce();
+    }else{
+      this.current_room_abs_id = this.four_by_four_move_room_service.current_room_reduce();
+    }
     
-    this.current_room_abs_id = this.move_room_service.current_room_reduce();
+    //this.current_room_abs_id = this.move_room_service.current_room_reduce();
     //console.log(this.current_room_abs_id)
     this.current_room_name = this.room_list[this.current_room_abs_id].namePretty;
     this.current_room_desc = this.room_list[this.current_room_abs_id].description;
